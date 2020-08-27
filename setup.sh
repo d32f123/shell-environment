9c44dce96d5cb38874723afa03e9509669b49933
Andrey Nesterov <anesterov@anesterov.xyz>
2020-08-28 00:39:25 +0300

    Add basic vim setup

--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@
 # https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
 BASE_DIR="$(pwd)"
 
+export XDG_CONFIG_HOME=$CONFIG_DIR
+export XDG_CACHE_HOME=$CACHE_DIR
+export XDG_DATA_HOME=$DATA_DIR
+
 export CONFIG_DIR="$HOME/.config"
 export ZDOTDIR="$CONFIG_DIR/zsh"
 export TMUX_HOME="$CONFIG_DIR/tmux"
@@ -70,4 +74,17 @@ cd "$BASE_DIR"
 # -- Set up tmux complete --- #
 
 # Set up vim TODO #
+mkdir -p "$XDG_DATA_HOME"/vim/{undo,swap,backup}
+
+#### VIM SETUP ####
+
+echo "set undodir=$XDG_DATA_HOME/vim/undo
+set directory=$XDG_DATA_HOME/vim/swap
+set backupdir=$XDG_DATA_HOME/vim/backup
+set viewdir=$XDG_DATA_HOME/vim/view
+set viminfo+='1000,n$XDG_DATA_HOME/vim/viminfo
+set runtimepath=$XDG_CONFIG_HOME/vim,$VIMRUNTIME,$XDG_CONFIG_HOME/vim/after" >>"$XDG_CONFIG_HOME"/vim/vimrc
+
+PROFILE_VIMINIT="export VIMINIT='source "'"$XDG_CONFIG_HOME/vim/vimrc"'"'"
+grep -q "$PROFILE_VIMINIT" $HOME/.profile || echo "$PROFILE_VIMINIT" >>$HOME/.profile
 echo "Please now do 'source $ZDOTDIR/.zshrc'" 
