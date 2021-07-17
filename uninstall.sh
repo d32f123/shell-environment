ed486c73be9340aa4a0dfe56716b3766e2535804
Andrey Nesterov <anesterov@anesterov.xyz>
2021-07-17 23:02:26 +0300

    Move tmux-plugin-remote-ip to github, stop uninstalling vim

--- uninstall.sh
+++ uninstall.sh
@@ -4,6 +4,6 @@ export CONFIG_DIR="$HOME/.config"
 export ZDOTDIR="$CONFIG_DIR/zsh"
 export TMUX_HOME="$CONFIG_DIR/tmux"
 export OH_MY_ZSH_HOME="$CONFIG_DIR/oh-my-zsh"
-export VIM_HOME="$CONFIG_DIR/vim"
 
-rm -rf "$ZDOTDIR" "$TMUX_HOME" "$OH_MY_ZSH_HOME" "$VIM_HOME"
+rm -rf "$ZDOTDIR" "$TMUX_HOME" "$OH_MY_ZSH_HOME"
+
